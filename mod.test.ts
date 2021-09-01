54ffc2b91055a5a3e7caf4be178985c1976defce
Eyal Shalev <eyalsh@gmail.com>
2021-09-01 22:35:28 +0300

    .

--- mod.test.ts
+++ mod.test.ts
@@ -1,63 +1,107 @@
 #!/usr/bin/env -S deno test --lock lock.test.json --import-map import_map.json
 
 import { assertEquals } from "deno/testing/asserts.ts";
-import { AsyncStack } from "./mod.ts";
+import { AsyncQueue } from "./mod.ts";
 
-Deno.test("pop -> push", async () => {
-  const stack = new AsyncStack<string>();
+Deno.test("no-buffer remove -> add", async () => {
+  const stack = new AsyncQueue<string>(0);
 
   assertEquals(
-    await Promise.all([stack.pop(), stack.push("a")]),
+    await Promise.all([stack.remove(), stack.add("a")]),
     ["a", undefined],
   );
 });
 
-Deno.test("push -> pop", async () => {
-  const stack = new AsyncStack<string>();
+Deno.test("no-buffer add -> remove", async () => {
+  const stack = new AsyncQueue<string>(0);
 
   assertEquals(
-    await Promise.all([stack.push("a"), stack.pop()]),
+    await Promise.all([stack.add("a"), stack.remove()]),
     [undefined, "a"],
   );
 });
 
-Deno.test("pop -> pop -> push -> push", async () => {
-  const stack = new AsyncStack<string>();
+Deno.test("no-buffer remove -> remove -> add -> add", async () => {
+  const stack = new AsyncQueue<string>(0);
 
   assertEquals(
     await Promise.all([
-      stack.pop(),
-      stack.pop(),
-      stack.push("a"),
-      stack.push("b"),
+      stack.remove(),
+      stack.remove(),
+      stack.add("a"),
+      stack.add("b"),
     ]),
-    ["a","b", undefined, undefined],
+    ["a", "b", undefined, undefined],
   );
 });
 
-Deno.test("push -> push -> pop -> pop", async () => {
-  const stack = new AsyncStack<string>();
+Deno.test("no-buffer add -> add -> remove -> remove", async () => {
+  const stack = new AsyncQueue<string>(0);
 
   assertEquals(
     await Promise.all([
-      stack.push("a"),
-      stack.push("b"),
-      stack.pop(),
-      stack.pop(),
+      stack.add("a"),
+      stack.add("b"),
+      stack.remove(),
+      stack.remove(),
     ]),
-    [undefined, undefined, "a","b"],
+    [undefined, undefined, "a", "b"],
   );
 });
 
-Deno.test("push -> pop; pop -> push", async () => {
-  const stack = new AsyncStack<string>();
+Deno.test("no-buffer add -> remove; remove -> add", async () => {
+  const stack = new AsyncQueue<string>(0);
 
   assertEquals(
-    await Promise.all([stack.push("a"), stack.pop()]),
-    [undefined, "a"]
-  )
+    await Promise.all([stack.add("a"), stack.remove()]),
+    [undefined, "a"],
+  );
   assertEquals(
-    await Promise.all([stack.pop(), stack.push("b")]),
-    ["b", undefined]
-  )
+    await Promise.all([stack.remove(), stack.add("b")]),
+    ["b", undefined],
+  );
+});
+
+Deno.test("buffered add -> remove", async () => {
+  const stack = new AsyncQueue<string>(1);
+
+  await stack.add("a");
+  assertEquals(await stack.remove(), "a");
+});
+
+Deno.test("buffered add -> add -> remove -> remove", async () => {
+  const stack = new AsyncQueue<string>(1);
+
+  await stack.add("a");
+
+  assertEquals(
+    await Promise.all([stack.add("b"), stack.remove()]),
+    [undefined, "a"],
+  );
+
+  assertEquals(await stack.remove(), "b");
+});
+
+Deno.test("buffered add -> remove; remove -> add", async () => {
+  const stack = new AsyncQueue<string>(1);
+
+  await stack.add("a");
+  assertEquals(await stack.remove(), "a");
+  await stack.add("a");
+  assertEquals(await stack.remove(), "a");
+});
+
+Deno.test("buffered add -> add -> add -> remove -> remove -> remove", async () => {
+  const stack = new AsyncQueue<string>(2);
+
+  await stack.add("a");
+  await stack.add("b");
+
+  assertEquals(
+    await Promise.all([stack.add("c"), stack.remove()]),
+    [undefined, "a"],
+  );
+
+  assertEquals(await stack.remove(), "b");
+  assertEquals(await stack.remove(), "c");
 });
--- mod.ts
+++ mod.ts
@@ -1,14 +1,16 @@
 export class InvalidTransitionError extends TypeError {
   constructor(state: State, t: Transition) {
     super(
-      `Invalid transition - ${JSON.stringify({ from: state.name, to: t })}`,
+      `Invalid transition - ${
+        JSON.stringify({ state: state.name, transition: t })
+      }`,
     );
   }
 }
 
 enum Transition {
-  POP = "POP",
-  PUSH = "PUSH",
+  REMOVE = "REMOVE",
+  ADD = "ADD",
   ACK = "ACK",
 }
 
@@ -20,44 +22,84 @@ type State = (t: Transition) => State;
 /**
  * @throws {InvalidTransitionError}
  */
-export function Empty(t: Transition): State {
-  if (t === Transition.POP) return PopStuck;
-  if (t === Transition.PUSH) return PushStuck;
-  throw new InvalidTransitionError(Empty, t);
+export function HasBuffer(t: Transition): State {
+  if (t === Transition.REMOVE) return RemoveStuck;
+  if (t === Transition.ADD) return AddStuck;
+  throw new InvalidTransitionError(HasBuffer, t);
 }
 
 /**
  * @throws {InvalidTransitionError}
  */
-export function PopStuck(t: Transition): State {
-  if (t === Transition.PUSH) return WaitingForAck;
-  throw new InvalidTransitionError(PopStuck, t);
+export function RemoveStuck(t: Transition): State {
+  if (t === Transition.ADD) return WaitingForAck;
+  throw new InvalidTransitionError(RemoveStuck, t);
 }
 
 /**
  * @throws {InvalidTransitionError}
  */
-export function PushStuck(t: Transition): State {
-  if (t === Transition.POP) return PopStuck;
-  throw new InvalidTransitionError(PushStuck, t);
+export function AddStuck(t: Transition): State {
+  if (t === Transition.REMOVE) return RemoveStuck;
+  throw new InvalidTransitionError(AddStuck, t);
 }
 
 /**
  * @throws {InvalidTransitionError}
  */
 export function WaitingForAck(t: Transition): State {
-  if (t === Transition.ACK) return Empty;
+  if (t === Transition.ACK) return HasBuffer;
   throw new InvalidTransitionError(WaitingForAck, t);
 }
 
 const scheduleTask = setTimeout;
 
-export class AsyncStack<T> {
-  protected current: State = Empty;
+export class Queue<T> {
+  protected queue: T[] = [];
+
+  constructor(readonly capacity: number) {
+    if (capacity < 0 || !Number.isSafeInteger(capacity)) {
+      throw new RangeError(
+        "queue capacity must be a non-negative safe integer",
+      );
+    }
+  }
+
+  add(val: T) {
+    if (this.isFull) throw new RangeError("queue is full");
+    this.queue.push(val);
+  }
+
+  remove(): T {
+    if (this.isEmpty) throw new RangeError("queue is empty");
+    return this.queue.shift() as T;
+  }
+
+  peek(): T {
+    if (this.isEmpty) throw new RangeError("queue is empty");
+    return this.queue[0] as T;
+  }
+
+  get isFull(): boolean {
+    return this.queue.length === this.capacity;
+  }
+
+  get isEmpty(): boolean {
+    return this.queue.length === 0;
+  }
+}
+
+export class AsyncQueue<T> {
+  protected current: State = HasBuffer;
   protected transitionEventTarget = new EventTarget();
   protected stateEventTarget = new EventTarget();
+  protected readonly queue: Queue<T>;
 
-  constructor(protected readonly options?: { debug?: boolean }) {
+  constructor(
+    capacity: number,
+    protected readonly options?: { debug?: boolean },
+  ) {
+    this.queue = new Queue<T>(capacity);
     if (options?.debug) {
       const reporter = (prefix: string) => {
         return (ev: Event) => {
@@ -70,46 +112,70 @@ export class AsyncStack<T> {
         this.transitionEventTarget.addEventListener(t, reporter("Transition"));
       });
 
-      [Empty, PopStuck, PushStuck, WaitingForAck].forEach((state) => {
+      [HasBuffer, RemoveStuck, AddStuck, WaitingForAck].forEach((state) => {
         this.stateEventTarget.addEventListener(state.name, reporter("State"));
       });
     }
   }
 
-  async pop(): Promise<T> {
-    if ([PopStuck, WaitingForAck].includes(this.current)) {
-      await this.waitForState(Empty);
+  async remove(): Promise<T> {
+    if ([RemoveStuck, WaitingForAck].includes(this.current)) {
+      await this.waitForState(HasBuffer);
     }
 
-    // Register to the WaitingForAck event before transitioning to guarantee order.
-    const valP = this.waitForState(WaitingForAck);
-
-    this.updateState(Transition.POP);
-
-    const val = await valP as T;
-
-    this.updateState(Transition.ACK);
-
-    return val;
-  }
-
-  async push(val: T) {
-    if ([PushStuck, WaitingForAck].includes(this.current)) {
-      await this.waitForState(Empty);
+    if (this.current === HasBuffer && !this.queue.isEmpty) {
+      return this.queue.remove();
     }
 
     // Register to the PopStuck event before transitioning to guarantee order.
-    const popStuckPromise = this.current === PopStuck
-      ? Promise.resolve()
-      : this.waitForState(PopStuck);
+    const waitForAckPromise = this.waitForState(WaitingForAck);
 
-    this.updateState(Transition.PUSH, val);
+    this.updateState(Transition.REMOVE);
+    const val = await waitForAckPromise as T;
+    this.updateState(Transition.ACK);
 
-    await popStuckPromise;
+    if (this.queue.isEmpty) return val;
 
-    if (this.current === PopStuck) {
-      this.updateState(Transition.PUSH, val);
+    const valToReturn = this.queue.remove();
+    this.queue.add(val);
+    return valToReturn;
+  }
+
+  async add(val: T): Promise<void> {
+    if ([AddStuck, WaitingForAck].includes(this.current)) {
+      await this.waitForState(HasBuffer);
     }
+
+    if (!this.queue.isFull) {
+      this.queue.add(val);
+      return;
+    }
+
+    // Register to the RemoveStuck event before transitioning to guarantee order.
+    const afterAddPromise = this.current === RemoveStuck
+      ? Promise.resolve()
+      : this.waitForState(RemoveStuck);
+
+    this.updateState(Transition.ADD, val);
+    if (this.current === HasBuffer) return;
+
+    await afterAddPromise;
+
+    if (this.current === RemoveStuck) {
+      return this.updateState(Transition.ADD, val);
+    }
+  }
+
+  peek(): T {
+    return this.queue.peek();
+  }
+
+  get isFull(): boolean {
+    return this.queue.isFull;
+  }
+
+  get isEmpty(): boolean {
+    return this.queue.isEmpty;
   }
 
   /**
