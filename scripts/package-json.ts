7705078c6aa33e554c65410fc834a338eb835359
Eyal Shalev <eyalsh@gmail.com>
2021-09-08 21:11:06 +0300

    .

--- scripts/package-json.ts
+++ scripts/package-json.ts
@@ -6,30 +6,32 @@ if (!valid(version)) {
   throw new TypeError(`${version} isn't a valid semver version`);
 }
 
+const scope = Deno.env.get("SCOPE") || "";
+if (!scope) {
+  throw new TypeError(`SCOPE environment variable is missing`);
+}
+
 const rootDir = dirname(join(fromFileUrl(import.meta.url), ".."));
 
 const name = basename(rootDir);
-const userName = "Eyal-Shalev";
-const repo = `${userName}/${name}`;
 
 const data = {
-  name: `@${repo.toLowerCase()}`,
+  name: `@${scope.toLowerCase()}/${name.toLowerCase()}`,
   version,
   description:
     "Inspired by Go & Clojure Channels, async-channels provides channels as an asynchronous communication method between asynchronous functions.",
   keywords: ["async", "channel", "channels", "await"],
-  homepage: `https://github.com/${repo}`,
+  homepage: `https://github.com/Eyal-Shalev/${name}`,
   bugs: {
-    url: `https://github.com/${repo}/issues`,
+    url: `https://github.com/Eyal-Shalev/${name}/issues`,
   },
   license: "GPL-3.0-only",
-  author: `Eyal Shalev <eyalsh@gmail.com> (https://github.com/${userName})`,
-  // files: [],
+  author: `Eyal Shalev <eyalsh@gmail.com> (https://github.com/Eyal-Shalev)`,
   main: "dist/cjs.bundle.js",
   module: "dist/es.bundle.js",
   repository: {
     type: "git",
-    url: `github:${repo}`,
+    url: `github:Eyal-Shalev/${name}`,
   },
   engines: {
     node: ">=14",
