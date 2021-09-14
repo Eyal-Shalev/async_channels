dcc388312e10a0e26ca2c6e04399f6e53b1a595f
Eyal Shalev <eyalsh@gmail.com>
2021-09-15 01:33:03 +0300

    ESM build process (#31)

--- scripts/bundle.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$DIR/.." && deno bundle --import-map "./import_map.json" "$DIR/../mod.ts" "$DIR/../dist/bundle.js"
