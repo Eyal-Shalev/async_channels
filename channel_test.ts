d7afbe60101a97e7e577d3b6fb77260346632cee
Eyal Shalev <eyalsh@gmail.com>
2021-09-23 23:21:33 +0300

    Split merge and select into their own files

--- channel_test.ts
+++ channel_test.ts
@@ -1,7 +1,7 @@
 import { sleep } from "./internal/utils.ts";
-import { Channel, merge, select } from "./channel.ts";
+import { Channel } from "./channel.ts";
 import { InvalidTransitionError } from "./internal/state-machine.ts";
-import { assertEquals, assertThrowsAsync, fail } from "deno/testing/asserts.ts";
+import { assertEquals, assertThrowsAsync } from "deno/testing/asserts.ts";
 
 Deno.test("no-buffer receive -> send", async () => {
   const chan = new Channel<string>(0);
@@ -125,48 +125,6 @@ Deno.test("send -> close -> send", async () => {
   assertThrowsAsync(() => chan.send("b"), InvalidTransitionError);
 });
 
-Deno.test("select when 1 channel is buffered", async () => {
-  const c1 = new Channel<string>(0);
-  const c2 = new Channel<string>(1);
-
-  const ctrl = new AbortController();
-
-  c1.send("c1", ctrl).then(() => fail("Should have failed"), () => {});
-  c2.send("c2", ctrl).catch((err) => fail(err));
-
-  const [val, selectedChannel] = await select([c1, c2]);
-  ctrl.abort();
-  assertEquals(selectedChannel, c2);
-  assertEquals(val, "c2");
-});
-
-Deno.test("select send when 1 has buffer", async () => {
-  const c1 = new Channel<string>(0);
-  const c2 = new Channel<string>(1);
-
-  const [val, selectedChannel] = await select([[c1, "c1"], [c2, "c2"]]);
-  assertEquals(val, true);
-  assertEquals(selectedChannel, c2);
-});
-
-Deno.test("select return default", async () => {
-  const c1 = new Channel<string>(0);
-  const c2 = new Channel<string>(0);
-
-  for (const expected of [undefined, null, true, false, 0, ""]) {
-    const [val, selectedChannel] = await select(
-      [[c1, "c1"], c2],
-      { default: expected },
-    );
-    assertEquals(val, expected);
-    assertEquals(selectedChannel, undefined);
-  }
-
-  c1.close();
-  c2.close();
-  await sleep(100);
-});
-
 Deno.test("channel as an async iterator", async () => {
   const chan = new Channel<string>(1);
 
@@ -220,29 +178,6 @@ Deno.test("flat", async () => {
   const expected = ["Hello", "world", "from", "Array"];
   for await (const x of flatCh) {
     assertEquals(x, expected.shift());
-  }
-
-  await p;
-  assertEquals(expected.length, 0, "expected stack isn't empty");
-});
-
-Deno.test("merge", async () => {
-  const ch1 = new Channel<string>();
-  const ch2 = new Channel<number>();
-  const mergedChan = merge([ch1, ch2]);
-
-  const p = Promise.all([
-    ch1.send("Hello"),
-    ch2.send(2),
-    ch1.send("world"),
-  ]).then(() => {
-    ch1.close();
-    ch2.close();
-  });
-
-  const expected = ["Hello", 2, "world"];
-  for await (const msg of mergedChan) {
-    assertEquals(msg, expected.shift());
   }
 
   await p;
