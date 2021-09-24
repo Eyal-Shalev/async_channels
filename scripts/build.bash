a7e3a5b89eddab9dcb79fded2f58ad15454f5f2b
Eyal Shalev <eyalsh@gmail.com>
2021-09-24 03:31:59 +0300

    fix build

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$DIR/.." && deno run --unstable --lock "$DIR/build-lock.json" --import-map "./import_map.json" --allow-write="./dist" --allow-read="." "$DIR/build.ts"
+cd "$DIR/.." && deno run --unstable --lock "$DIR/build-lock.json" --import-map "./import_map.json" --allow-write="./dist" --allow-read="." "$DIR/build.ts" "$@"
