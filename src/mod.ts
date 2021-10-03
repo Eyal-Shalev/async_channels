e38ebf1d92175f72b2e11eb5186343c7ef82d6e3
Eyal Shalev <eyalsh@gmail.com>
2021-10-03 23:14:58 +0300

    Moved select_template to tagged/select

--- src/mod.ts
+++ src/mod.ts
@@ -2,5 +2,6 @@ export * from "./channel.ts";
 export * from "./broadcast.ts";
 export * from "./select.ts";
 export * from "./merge.ts";
+export * as tagged from "./tagged/mod.ts";
 export * as pipe from "./pipe.ts";
 export * as time from "./time.ts";
